fcbb2f1d71421ef7b3578db502bea62af355da63
Shawn Garner <shawndgarner@gmail.com>
2015-02-05 13:17:38 +0000

    completed sorted exercise

---
+++ src/main/scala/net/nomadicalien/ch2/Sorted.scala
@@ -0,0 +1,30 @@
+package net.nomadicalien.ch2
+
+import scala.annotation.tailrec
+
+/**
+ * Created by Shawn on 2/5/2015.
+ */
+object Sorted {
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    val arraySize = as.size
+    if(arraySize == 0 || arraySize == 1) {
+      true
+    } else {
+      val arraySizeMinus1 = arraySize - 1
+      @tailrec
+      def isSortedIter(index: Int): Boolean = {
+        if(index == arraySizeMinus1) {
+          true
+        } else {
+          ordered(as(index), as(index+1)) && isSortedIter(index + 1)
+        }
+      }
+
+      isSortedIter(0)
+    }
+  }
+
+
+}

692821d5bd345b1ac972024ed4bf7e94d514a192
Shawn Garner <shawndgarner@gmail.com>
2015-03-08 08:03:52 +0100

    added a recursive method to verify the first n random numbers are between (inclusive) 0 and Int MaxValue

--- src/test/scala/net/nomadicalien/ch6/SimpleRNGTest.scala
+++ src/test/scala/net/nomadicalien/ch6/SimpleRNGTest.scala
@@ -9,10 +9,17 @@ class SimpleRNGTest extends FunSuite with Matchers {
   import RNG._
   test("exercise 6.1, nonNegativeInt via nextInt") {
     val rng = new SimpleRNG(20l)
-    val (n1, rnd1) = nonNegativeInt(rng)
-    n1 should be(7694978)
-    val (n2, rnd2) = nonNegativeInt(rnd1)
-    n2 should be(1630622802)
+
+    def validatePositive(r: RNG, n: Int):Unit = {
+      val (randN, newRNG) = nonNegativeInt(r)
+      println(randN)
+      randN should (be >= 0 and be <= Int.MaxValue)
+      if(n > 0) {
+        validatePositive(newRNG, n-1)
+      }
+    }
+
+    validatePositive(rng, 1000)
   }
 
 

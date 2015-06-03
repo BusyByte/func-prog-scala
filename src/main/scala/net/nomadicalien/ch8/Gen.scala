9fe47c98b19ac18e09d12cbd0fba46f39bf01e4c
Shawn Garner <shawndgarner@gmail.com>
2015-06-04 00:20:51 +0100

    implemented up through exercise 8.8

--- src/main/scala/net/nomadicalien/ch8/Gen.scala
+++ src/main/scala/net/nomadicalien/ch8/Gen.scala
@@ -61,6 +61,17 @@ object Gen {
   else
     g2
   }
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val cutoff = g1._2 / (g1._2 + g2._2)
+    Gen(State(RNG.double)).flatMap { d =>
+      if(d > cutoff)
+        g2._1
+      else
+        g1._1
+
+    }
+  }
 }
 case class Gen[A](sample: State[RNG,A]) {
   def flatMap[B](f: A => Gen[B]): Gen[B] =

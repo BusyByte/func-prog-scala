f06c0df69240c643955205094bec8d99f8689e34
Shawn Garner <shawndgarner@gmail.com>
2015-06-05 11:58:27 +0100

    added code for listing 8.5

--- src/main/scala/net/nomadicalien/ch6/RNG.scala
+++ src/main/scala/net/nomadicalien/ch6/RNG.scala
@@ -1,5 +1,7 @@
 package net.nomadicalien.ch6
 
+import _root_.fpinscala.state.RNG.Simple
+
 
 /**
  * Created by Shawn on 3/7/2015.
@@ -21,6 +23,15 @@ case class SimpleRNG(seed: Long) extends RNG {
 
 object RNG {
   type Rand[+A] = RNG => (A, RNG)
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    }
+  }
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (nextN, nextRng) = rng.nextInt
--- src/main/scala/net/nomadicalien/ch8/Gen.scala
+++ src/main/scala/net/nomadicalien/ch8/Gen.scala
@@ -99,6 +99,17 @@ object Prop {
         }).toList.reduce(_ && _)
       prop.run(max,n,rng)
   }
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+    }
 }
 
 object Gen {
